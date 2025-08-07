8787fd534214f264705b0f70335e087378cd09d9
H_KIcom <>
2025-08-07 12:03:07 +0900

    folder구조 변경 merge into main
    # Conflicts:
    #	Assets/_Proj/Scripts/InputSystem/BaseInput.cs
    #	Assets/_Proj/Scripts/InputSystem/BaseInput.cs.meta
    #	Assets/_Proj/Scripts/InputSystem/KeyboardInput.cs
    #	Assets/_Proj/Scripts/InputSystem/KeyboardInput.cs.meta
    #	Assets/_Proj/Scripts/Player/PowerUpEffect.cs
    #	Assets/_Proj/Scripts/Player/PowerUpEffect.cs.meta
    #	Assets/_Proj/Scripts/Player/Stats.cs
    #	Assets/_Proj/Scripts/Player/Stats.cs.meta

---
+++ Assets/_Proj/Scripts/Player/PowerUpEffect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+[Serializable]
+public class PowerUpEffect
+{
+  public float boostSpeed;
+  public float boostAccel;
+  public float driftSteer;
+  public float barrelSpeed;
+  public float barrelAccel;
+  public float duration;
+  public float elapsedTime;
+
+  public Stats modifier;
+}
---
+++ Assets/_Proj/Scripts/Player/Stats.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Stats
+{
+  [Header("Movement Settings")]
+  public float currSpeed;
+  public float speed = 20f;
+  public float acceleration = 10f;
+  public float steer = 100f;
+  public float brakeForce = 30f;
+  [Min(0.001f)]
+  public float limitSpeed;
+  [Min(0.001f)]
+  public float reverseSpeed;
+  public float reverseAccel;
+  [Tooltip("The speed at which the car starts accelerating from 0.")]
+  [Range(0.2f, 1)]
+  public float AccelerationCurve;
+  public float braking;
+  [Tooltip("The time it takes for the car to come to a complete stop when there is no input.")]
+  public float dragTime;
+  [Range(0.0f, 1.0f)]
+  [Tooltip("The amount of side-to-side friction.")]
+  public float grip;
+  [Tooltip("Additional gravity for when the car is in the air.")]
+  public float addedGravity;
+
+  public static Stats operator +(Stats a, Stats b)
+  {
+    return new Stats
+    {
+      acceleration = a.acceleration + b.acceleration,
+      limitSpeed = a.limitSpeed + b.limitSpeed,
+      reverseSpeed = a.reverseSpeed + b.reverseSpeed,
+      reverseAccel = a.reverseAccel + b.reverseAccel,
+      braking = a.braking + b.braking,
+      steer = a.steer + b.steer,
+      grip = a.grip + b.grip,
+      addedGravity = a.addedGravity + b.addedGravity
+    };
+  }
+}
