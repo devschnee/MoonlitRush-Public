d83c7082e9340bfd60d5f1f75f73493d57887bf8
seosuha <zizihamzixi@gmail.com>
2025-08-14 18:37:47 +0900

    검토완료 (chatgpt이미지 상업/비상업 사용가능)
    skybox 원본 이미지X (베이크된 것만 커밋)

---
+++ Assets/_Proj/Scripts/Player/BoostFX.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostFX : MonoBehaviour
+{
+  [Header("FX Reference")]
+  public ParticleSystem boostTrail; // Exhaust_Trail 프리팹 연결하면 됨
+
+  [Header("Normal State")]
+  public Color normalColor = Color.white;
+  public float normalStartSize = 0.4f;
+  public float normalStartSpeed = 2f;
+
+  [Header("Boost State")]
+  public Color boostColor = Color.cyan;
+  public float boostStartSize = 1f;
+  public float boostStartSpeed = 5f;
+
+  private bool isBoosting = false;
+  private float boostTimer;
+
+  ParticleSystem.MainModule main;
+
+  void Start()
+  {
+    ApplyNormalState();
+  }
+
+  void Update()
+  {
+    if (isBoosting)
+    {
+      boostTimer -= Time.deltaTime;
+      if (boostTimer <= 0f)
+      {
+        EndBoost();
+      }
+    }
+  }
+
+  public void StartBoost(float duration)
+  {
+    boostTimer = duration;
+    isBoosting = true;
+
+    ApplyBoostState();
+  }
+
+  private void EndBoost()
+  {
+    isBoosting = false;
+    ApplyNormalState();
+  }
+
+  private void ApplyNormalState()
+  {
+    if (boostTrail != null)
+    {
+      var main = boostTrail.main;
+      main.startColor = normalColor;
+      main.startSize = normalStartSize;
+      main.startSpeed = normalStartSpeed;
+    }
+  }
+
+  private void ApplyBoostState()
+  {
+    if (boostTrail != null)
+    {
+      var main = boostTrail.main;
+      main.startColor = boostColor;
+      main.startSize = boostStartSize;
+      main.startSpeed = boostStartSpeed;
+    }
+  }
+}
